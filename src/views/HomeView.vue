0ec82f85f358ec8a14e1d88240d7a3972d2fe61f
Fanoo211 <ternavasiofano@gmail.com>
2023-08-23 18:53:21 -0300

    Mejorando home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    <h1 class="text">Bienvenido, {{ usuario }}!</h1>
+    <div class="row">
+
+      <div class="col s12 m8 l8 "> <!-- Note that "m4 l3" was added -->
+        <div class="text-bienvenida">
+          <h1>{{ textoMostrar }}</h1>
+        </div>
+
+        <div class="text-home">
+          <h4>En el emocionante mundo de las criptomonedas, ¡somos tu puerta de entrada para comprar y vender con confianza!</h4>
+          <h4>Ya seas un entusiasta experimentado o estés dando tus primeros pasos en el universo digital de las monedas, en CriptoFano te acompañamos en cada paso del camino.</h4>
+
+                  <div class="imagen-criptos">
+          <img src="../assets/cripto-images/btc.png" alt="btc" class="btc">
+          <img src="../assets/cripto-images/eth.png" alt="eth" class="eth">
+          <img src="../assets/cripto-images/dai.png" alt="dai" class="dai">
+          <img src="../assets/cripto-images/usdt.png" alt="usdt" class="usdt">
+        </div>
+        </div>
+      </div>
+
+      <div class="col s12 m4 l4 "> <!-- Note that "m8 l9" was added -->
+        <div class="imagen-home">
+          <img src="../assets/homeFondo.png" alt="home" class="imagen">
+        </div>
+      </div>
+
+      <div class="col s12 m12 l12 ">
+
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -9,25 +39,66 @@ import { useAuthStore } from '../store/auth.js';
 
 export default {
   name: 'HomeView',
+  data() {
+    return{
+      textoMostrar: "",
+    };
+  },
+  mounted() {
+    this.mostrarLetraPorLetra();
+  },
   computed: {
     usuario() {
       const authStore = useAuthStore();
       return authStore.usuario;
     }
   },
+  methods: {
+    mostrarLetraPorLetra() {
+      let textoBienvenida = `Bienvenido, ${this.usuario}!`;
+      let index = 0;
+      const interval = setInterval(() => {
+        if(index === textoBienvenida.length) {
+          clearInterval(interval);
+          return;
+        }
+        this.textoMostrar += textoBienvenida[index];
+        index++;
+      }, 100);
+    },
+  }
 }
 </script>
 
-
 <style scoped>
-@font-face {
-  font-family: 'Vercetti Regular';
-  src: url('~@/assets/fonts/Vercetti-Regular.otf') format('otf'), /* Ruta relativa al archivo de tipografía */
-       /* Agrega más formatos si es necesario */
+.imagen {
+  width: 400px;
+  height: 450px;
 }
 
-.text{
-  font-family: 'Vercetti Regular';
-  font-size: 100px;
+.btc {
+  width: 200px;
+  height: 100px;
+}
+.eth {
+  width: 200px;
+  height: 130px;
+}
+.dai {
+  width: 200px;
+  height: 100px;
+  padding-bottom: 20px;
+}
+.usdt {
+  width: 160px;
+  height: 85px;
+  padding-bottom: 35px;
+  padding-left: 10px;
+}
+
+.text-bienvenida {
+  font-family: 'Lilita One', cursive;
 }
 </style>
+
+
