4e48e1b19344b804a02dc3aaeea1beae82a2b910
Fanoo211 <ternavasiofano@gmail.com>
2023-08-30 12:01:13 -0300

    Volver para atras

--- src/router/index.js
+++ src/router/index.js
@@ -10,38 +10,61 @@ const routes = [
   {
     path: '/HomeView',
     name: 'HomeView',
-    component: HomeView
+    component: HomeView,
+    meta: { scrollY: 0 },
   },
   {
     path: '/CompraView',
     name: 'CompraView',
     component: CompraView,
+    meta: { scrollY: 0 },
   },
   {
     path: '/VentaView',
     name: 'VentaView',
     component: VentaView,
+    meta: { scrollY: 0 },
   },
   {
     path: '/MovimientosView',
     name: 'MovimientosView',
     component: MovimientosView,
+    meta: { scrollY: 0 },
   },
   {
     path: '/EstadoActualView',
     name: 'EstadoActualView',
     component: EstadoActualView,
+    meta: { scrollY: 0 },
   },
   {
     path: '/PoliticasView',
     name: 'PoliticasView',
     component: PoliticasView,
+    meta: { scrollY: 0 },
   },
 ]
 
+const validRoutes = routes.map(route => route.name);
+
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.meta.scrollY) {
+      return { top: to.meta.scrollY };
+    } else {
+      return { top: 0 };
+    }
+  },
+});
+
+router.afterEach((to, from) => {
+  if (from.meta && validRoutes.includes(from.name)) {
+    from.meta.scrollY = window.scrollY;
+  }
+});
 
 export default router;
