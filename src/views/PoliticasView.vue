be810587b39e0f9b3dfd668620ed8de81f727fd7
Fanoo211 <ternavasiofano@gmail.com>
2023-08-30 12:00:54 -0300

    Politicas mejorado

--- src/views/PoliticasView.vue
+++ src/views/PoliticasView.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="container">
   <h2>Política de Privacidad</h2>
-  <p><strong>Última actualización:</strong> [Fecha de la última actualización]</p>
-  <p>Bienvenido/a a [Nombre del Sitio Web] ("nosotros", "nuestro", o "sitio web"). Esta página le informa sobre nuestras políticas con respecto a la recopilación, uso y divulgación de la Información Personal que recibimos de los usuarios del sitio web.</p>
+  <p><strong>Última actualización:</strong> 28/08/2023 </p>
+  <p>Bienvenido/a a CriptoFano. Esta página le informa sobre nuestras políticas con respecto a la recopilación, uso y divulgación de la Información Personal que recibimos de los usuarios del sitio web.</p>
   <p>Utilizamos su Información Personal solamente para proporcionar y mejorar el sitio. Al usar el sitio, usted acepta la recopilación y el uso de la información de acuerdo con esta política.</p>
   <h3>Recopilación y Uso de la Información</h3>
   <p>Mientras utiliza nuestro sitio, podemos pedirle que nos proporcione cierta información personalmente identificable que se puede utilizar para contactarlo o identificarlo. La información personalmente identificable puede incluir, pero no se limita a, su nombre ("Información Personal").</p>
@@ -12,7 +12,7 @@
   <h3>Seguridad</h3>
   <p>La seguridad de su Información Personal es importante para nosotros, pero recuerde que ningún método de transmisión a través de Internet o método de almacenamiento electrónico es 100% seguro. Aunque nos esforzamos por utilizar medios comercialmente aceptables para proteger su Información Personal, no podemos garantizar su seguridad absoluta.</p>
   <h3>Cambios a esta Política de Privacidad</h3>
-  <p>Esta Política de Privacidad es efectiva a partir de [Fecha de la última actualización] y permanecerá en vigencia, excepto con respecto a cualquier cambio en sus disposiciones en el futuro, que entrará en vigencia inmediatamente después de su publicación en esta página.</p>
+  <p>Esta Política de Privacidad es efectiva a partir de 28/08/2023 y permanecerá en vigencia, excepto con respecto a cualquier cambio en sus disposiciones en el futuro, que entrará en vigencia inmediatamente después de su publicación en esta página.</p>
   <p>Nos reservamos el derecho de actualizar o cambiar nuestra Política de Privacidad en cualquier momento y usted debe verificar esta Política de Privacidad periódicamente. Su uso continuado del servicio después de que publiquemos cualquier modificación a la Política de Privacidad en esta página constituirá su reconocimiento de las modificaciones y su consentimiento para acatar y estar sujeto a la Política de Privacidad modificada.</p>
   <h3>Contacto</h3>
   <p>Si tiene alguna pregunta sobre esta Política de Privacidad, por favor contáctenos.</p>
@@ -22,5 +22,13 @@
 <script>
 export default {
   name: 'PoliticasView',
+  mounted() {
+    this.scrollToTop();
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  }
 }
 </script>
