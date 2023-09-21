b5ade66e3807b15a1aadba6b9b615644f7e627b2
Fanoo211 <ternavasiofano@gmail.com>
2023-09-21 16:51:38 -0300

    cambios home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row">
 
-      <div class="col s12 m8 l8 "> <!-- Note that "m4 l3" was added -->
+      <div class="col s12 m8 l8 ">
         <div class="text-bienvenida">
           <h1 style="font-size: 80px">{{ textoMostrar }}</h1>
         </div>
@@ -11,23 +11,19 @@
           <h4>En el emocionante mundo de las criptomonedas, ¡somos tu puerta de entrada para comprar y vender con confianza!</h4>
           <h4>Ya seas un entusiasta experimentado o estés dando tus primeros pasos en el universo digital de las monedas, en CriptoFano te acompañamos en cada paso del camino.</h4>
 
-                  <div class="imagen-criptos">
-          <img src="../assets/cripto-images/btc.png" alt="btc" class="btc">
-          <img src="../assets/cripto-images/eth.png" alt="eth" class="eth">
-          <img src="../assets/cripto-images/dai.png" alt="dai" class="dai">
-          <img src="../assets/cripto-images/usdt.png" alt="usdt" class="usdt">
-        </div>
+          <div class="imagen-criptos">
+            <img src="../assets/cripto-images/btc.png" alt="btc" class="btc">
+            <img src="../assets/cripto-images/eth.png" alt="eth" class="eth">
+            <img src="../assets/cripto-images/dai.png" alt="dai" class="dai">
+            <img src="../assets/cripto-images/usdt.png" alt="usdt" class="usdt">
+          </div>
         </div>
       </div>
 
-      <div class="col s12 m4 l4 "> <!-- Note that "m8 l9" was added -->
+      <div class="col s12 m4 l4 ">
         <div class="imagen-home">
           <img src="../assets/homeFondo.png" alt="home" class="imagen">
         </div>
-      </div>
-
-      <div class="col s12 m12 l12 ">
-
       </div>
 
     </div>
